3ab15085853714fd40f194781a6b7c9b07570398
lbarallobre <lucas.barallobre@ortex.com>
2023-05-16 17:07:08 -0300

    Build a custom hook to get and save in local storage

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react";
+
+export function useLocalStorage(
+  key: string,
+  defaultValue = "",
+  { serialize = JSON.stringify, deserialize = JSON.parse } = {}
+) {
+  const [state, setState] = useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key);
+    if (valueInLocalStorage) {
+      try {
+        return deserialize(valueInLocalStorage);
+      } catch (error) {
+        window.localStorage.removeItem(key);
+      }
+    }
+    return defaultValue;
+  });
+
+  const prevKeyRef = useRef(key);
+
+  useEffect(() => {
+    const prevKey = prevKeyRef.current;
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey);
+    }
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(state));
+  }, [key, state, serialize]);
+
+  return [state, setState];
+}
